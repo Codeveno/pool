075b7993420b1d714fa98f2dfedee6ba17cb1c98
Codeveno <samsonochieng476@gmail.com>
2025-03-15 21:33:46 +0300

    Add files via upload

---
+++ Artifacts.java
@@ -0,0 +1,21 @@
+public class Artifacts {
+    public static void main(String[] args) {
+        // 1. Declare an int variable named artifactsFound and initialize it with the value 25
+        int artifactsFound = 25;
+
+        // 2. Declare an int variable named day and initialize it with the value 10
+        int day = 10;
+
+        // 3. Display a message in the following format
+        System.out.println("Found " + artifactsFound + " artifacts on day " + day);
+
+        // 4. Assign the value 32 to artifactsFound
+        artifactsFound = 32;
+
+        // 5. Assign the value 16 to day
+        day = 16;
+
+        // 6. Display the same message that you displayed in Step 3
+        System.out.println("Found " + artifactsFound + " artifacts on day " + day);
+    }
+}
